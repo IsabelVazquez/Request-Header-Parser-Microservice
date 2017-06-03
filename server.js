eee00fb5df35a017e16874c9f4c6e8e0c7fc7850
Isabel <12ivazquez@gmail.com>
2017-06-03 23:08:29 +0000

    [#2/Isabel] IP and language work

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-var express = require('express');
-const app = express()
+const express = require('express');
+const app = express();
+const parser = require('accept-language-parser');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  var languages = parser.parse(req.headers['accept-language']).toAgent();
+
+  res.send(JSON.stringify({
+    ipaddress: req.headers['x-forwarded-for'],
+    language: languages[0]["code"] + '-' + languages[0]["region"],
+    //need more work
+    software: req.headers['user-agent']
+  }))
 })
 
 app.listen(8080, function () {
