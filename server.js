36ab7e2b4f921e939d453f3965a6fbbebbc9cd74
Isabel <12ivazquez@gmail.com>
2017-06-04 03:25:26 +0000

    [#3/Isabel] User story complete

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 const parser = require('accept-language-parser');
+var useragent = require('useragent');
 
 app.get('/', function (req, res) {
-  var languages = parser.parse(req.headers['accept-language']).toAgent();
-
+  var languages = parser.parse(req.headers['accept-language']);
+  var agent = useragent.parse(req.headers['user-agent']);
+  var version = agent.os.toVersion();
+  
   res.send(JSON.stringify({
     ipaddress: req.headers['x-forwarded-for'],
     language: languages[0]["code"] + '-' + languages[0]["region"],
-    //need more work
-    software: req.headers['user-agent']
+    software: agent['os']['family'] + ' ' + version
   }))
 })
 
